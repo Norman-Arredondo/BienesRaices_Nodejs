c2ba8dd60f0258d32a137ff6f23bd668c9acf0a8
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-22 01:03:47 -0600

    Routing
    Definir routing

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require('express');
 //Crear la app. Mandamos a llamar
 const app = express(); 
 
-
+//Routing
+app.get('/', function(req, res){
+    res.send('Hola mundo en Express');
+});
 
 //Definir un puerto y arrancar el proyecto
-const port = 3001; 
+const port = 3000; 
 app.listen(port, () => {
     console.log(` El servidor está funcionando en el puerto ${port}`)
 });
