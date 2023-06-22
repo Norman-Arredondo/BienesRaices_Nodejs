54a2b4a4df18fc93d847ff3ed2225a83fd3eca7b
Norman Arredondo <arredondo_norman25@outlook.es>
2023-06-22 00:36:59 -0600

    Iniciando servidor
    Express

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log('Hello World')
-console.log(2+2)
+//Importar Express. Lo extrae, loasigna a una variable
+const express = require('express');
+
+//Crear la app. Mandamos a llamar
+const app = express(); 
+
+
+
+//Definir un puerto y arrancar el proyecto
+const port = 3001; 
+app.listen(port, () => {
+    console.log(` El servidor está funcionando en el puerto ${port}`)
+});
