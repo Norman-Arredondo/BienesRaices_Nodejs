8bbbcf675622ae0f092ba16e3edd10b937eaf094
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-02 01:04:26 -0600

    layout principal
    Instalación tailwind
    Vistas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['/view/**/*.pug'],
+module.exports = {
+  content: ['./views/**/*.pug'],
   theme: {
     extend: {},
   },
