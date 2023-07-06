806cd93fa5daad577a4fabd0bae6bc2b699cac0f
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-06 00:43:29 -0600

    Validaciones
    express-validator

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -19,7 +19,13 @@ const formularioRegistro =  (req, res ) => {
 const registrar = async (req, res) =>{
     //Validación
     await check('nombre').notEmpty().withMessage('El campo nombre no puede ir vacío').run(req);
+    await check('email').isEmail().withMessage('Ingresa un Email válido').run(req);
+    await check('password').isLength({min: 6, max: 6}).withMessage('La contraseña debe de ser de exactamente 6 caracteres').run(req);
+    await check('repetir_password').equals('password').withMessage('La contraseña no es la misma').run(req);
+
     let resultado = validationResult(req);
+
+    //Verificar que el resultado este vacío
     res.json(resultado.array());
     const usuario = await Usuario.create(req.body)
     res.json(usuario)
