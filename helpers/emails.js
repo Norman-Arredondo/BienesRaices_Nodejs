d72d79e1a8e1f75a6d44c720be424efa9562d6da
Norman Arredondo <arredondo_norman25@outlook.es>
2023-07-07 13:27:58 -0600

    NodeMailer

---
+++ helpers/emails.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+
+const emailRegistro = async (datos) => {
+
+}
+
+export {
+    emailRegistro
+}
